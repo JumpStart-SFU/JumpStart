45b79a40df673bbf0c1e6d0e8c51b302a9c82aef
Choong Jin Ng <jin_9393@hotmail.com>
2015-08-08 02:04:26 -0700

    Able to login like a user

--- include/database/database-users.php
+++ include/database/database-users.php
@@ -10,21 +10,53 @@
  * FALSE if otherwise
  */
 function db_table_user_insert($conn, $table, $data) {
-  $username = $data['username'];
-  $fullname = $data['fullname'];
-  $password = password_hash($data['password'], PASSWORD_BCRYPT); // hash this
-  $sex = $data['sex'];
-  $interest_1 = $data['interest-1'];
-  $interest_2 = $data['interest-2'];
-  $interest_3 = $data['interest-3'];
-  
-  $query = sanitize_MySQL($conn, "INSERT INTO $table VALUES ('$username', '$fullname', '$password', '$sex', '$interest_1', '$interest_2', '$interest_3')");
-  
-  $result = $conn->query($query);
-  
-  if (!($result)) {
-    die("Database access failed: " . $conn->error . "<br />");
+  if ($conn->connect_error) {
+    print "Something went wrong with the connection<br/>";
   }
+  
+  if (!(empty($table) && empty($data))) {
+    $username = $data['username'];
+    $fullname = $data['fullname'];
+    $password = password_hash($data['password'], PASSWORD_BCRYPT);
+    $sex = $data['sex'];
+    $interest_1 = $data['interest-1'];
+    $interest_2 = $data['interest-2'];
+    $interest_3 = $data['interest-3'];
+  
+    $query = sanitize_MySQL($conn, "INSERT INTO $table VALUES ('$username', '$fullname', '$password', '$sex', '$interest_1', '$interest_2', '$interest_3')");
+  
+    $result = $conn->query($query);
+  
+    if (!($result)) {
+      die("Database access failed: " . $conn->error . "<br />");
+    }
+  }
+}
+
+/**
+ * Reads the user's data (but not password) based on their username and passwprd
+ * Gets the user details based 
+ */
+function db_table_user_read($conn, $table, $username, $password) {
+  if ($conn->connect_error) {
+    print "Something went wrong with the connection<br/>";
+  }
+  
+  if (!(empty($table) && empty($user_data) && empty($column))) {
+    $password = password_hash($password, PASSWORD_BCRYPT);
+    
+    $query = "SELECT username, fullname, sex, interest_1, interest_2, interest_3 ";
+    $query .= "FROM $table WHERE username = '$username' AND password = '$password'";
+    $result = NULL;
+    $result = $conn->query($query);
+    
+    var_dump($query);
+
+    return $result;
+  }
+}
+
+function db_table_user_readAll($conn, $table, $username) {
 }
 
 /**
--- profile.php
+++ profile.php
@@ -5,28 +5,42 @@
 
 require_once('include/database/database-basic.php');
 require_once('include/database/database-users.php');
+require_once('include/database/validate.php');
 
-db_connect();
+$conn = db_connect();
 $activate = TRUE;
 
 $email = sanitize_string($_POST['email']);
-$password = sanitize_string($_POST['password']);
+$password = sanitize_MySQL($conn, $_POST['password']);
 
 if (substr($email, -6) === '@sfu.ca') {
-  $username = substr($email, 0, -6);
+  $username = sanitize_MySQL($conn, substr($email, 0, -6));
+}
+
+$result = db_table_user_read($conn, 'users_sfu', $password);
+
+if ($result) {
+  var_dump($result);
+  print $result->username;
+  
+  print $result->username;
+}
+
+else {
+  print 'no result';
 }
 
 
 
-
-
 // Verify activation
+/*
 if (!($activate)) {
   
   // redirect to activation page
   header("Location: activate.php");
   exit;
 }
+*/
 
 ?>
 
