11e2a1e70a05db93a3c16372557c39b798e0a187
Choong Jin Ng <jin_9393@hotmail.com>
2015-08-09 19:32:55 -0700

    Validate form using PHP
    Validates the form using PHP. JavaScript implementation is ongoing.

---
+++ js/register.js
@@ -0,0 +1,31 @@
+function validateUsername(username) {
+  return (username == "") ? "Username is empty.\n" : "";
+}
+
+function validatePassword(password_1, password_2) {
+  if (password_1 == "") {
+    return "No password was entered.\n";
+  }
+  
+  else if (password_1.length < 8) {
+    return "Passwords must be at least 8 characters long. \n";
+  }
+  
+  else if (!/[a-z]/.test(password_1) || ! /[A-Z]/.test(password_1) || !/[0-9]/.test(password_1)) {
+    return "Passwords require at least one lowercase, one uppercase and one number.\n";
+  }
+  
+  else if (password_1 !== password_2) {
+    return "Your passwords do not mtach.\n";
+  }
+  
+  return "";
+}
+
+function validateFullname(fullname) {
+  return (fullname == "") ? "Full name is empty.\n" : "";
+}
+
+function validateSex(sex) {
+  return (sex == "") ? "Sex is empty.\n" : "";
+}
