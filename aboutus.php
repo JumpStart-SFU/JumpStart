ccb3ed7099e283f17dce3b79987946d9e0e7beb9
Choong Jin Ng <jin_9393@hotmail.com>
2015-10-19 21:58:40 -0700

    Created an About Us page placeholder

---
+++ aboutus.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- META STUFF -->
+  <meta name="description" content="Meet new friends on your local campus!" />
+  <meta name="viewport" content="width-device-width, initial-scale=1" />
+  <title>About Us | Conextus</title>
+  <!-- CSS Stuff -->
+  <link rel="stylesheet" type="text/css" href="css/index.css" />
+  <link rel="shortcut icon" href="http://sstatic.net/stackoverflow/img/favicon.ico">
+</head>
+<body>
+  <a href="index.php">Home</a>
+  <?php include_once 'include/partial/footer.php'; ?>
+</body>
+</html>
--- include/partial/footer.php
+++ include/partial/footer.php
@@ -11,6 +11,7 @@
     <a href="">Terms &amp; Conditons</a>
     <a href="">Privacy Policy</a>
     <a href="">Cookie Information</a>
+    <a href="aboutus.php">About Us</a>
     <p>Copyright</p>
   </div>
 </footer>
